bfc7d3f86c0f47f533107b24ff5dfc6fa2345cba
DStadnyk26 <165746849+DStadnyk26@users.noreply.github.com>
2024-04-04 13:31:56 -0600

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,5 +1,13 @@
 let running = false;
 
+let cashSpeed = 1 * 60 * 1000);
+let forgerySpeed = 1 * 60 * 1000);
+let weedSpeed = 1 * 60 * 1000);
+let methSpeed = 1 * 60 * 1000);
+let cokeSpeed = 1 * 60 * 1000);
+let acidSpeed = 1 * 60 * 1000);
+let bunkerSpeed = 5 * 60 * 1000);
+
 export let userInfo = {
     
     bunker: {
@@ -7,7 +15,7 @@ export let userInfo = {
         product: 0,
         productMax: 100,
         research: 0,
-        researchMax: 0,
+        researchMax: 60,
         supplies: 0,
         suppliesMax: 100,
         upgrades: {
@@ -21,43 +29,113 @@ export let userInfo = {
         },
     },
     
+    cash: {
+        owned: false,
+        product: 0,
+        productMax: 20,
+        supplies: 0,
+        suppliesMax: 100,
+        upgrades: {
+            equipment: false,
+            staff: false,
+            security: false,
+        },
+    },
+    
+    forgery: {
+        owned: false,
+        product: 0,
+        productMax: 50,
+        supplies: 0,
+        suppliesMax: 100,
+        upgrades: {
+            equipment: false,
+            staff: false,
+            security: false,
+        },
+    },
+    
+    weed: {
+        owned: false,
+        product: 0,
+        productMax: 36,
+        supplies: 0,
+        suppliesMax: 100,
+        upgrades: {
+            equipment: false,
+            staff: false,
+            security: false,
+        },
+    },
+    
+    meth: {
+        owned: false,
+        product: 0,
+        productMax: 8,
+        supplies: 0,
+        suppliesMax: 100,
+        upgrades: {
+            equipment: false,
+            staff: false,
+            security: false,
+        },
+    },
+    
+    coke: {
+        owned: false,
+        product: 0,
+        productMax: 4,
+        supplies: 0,
+        suppliesMax: 100,
+        upgrades: {
+            equipment: false,
+            staff: false,
+            security: false,
+        },
+    },
+}
+function bikerUpdate(const path) {
+    if (!running || path.owned || path.supplies <= 0 || path.product == path.productMax) return;
+    
+    path.product ++;
+    path.supplies --;
     
 }
 
 function bunkerUpdate() {
-    let path = userInfo.bunker;
-    
-    if(running == true) {
-        if(path.owned == true) {
-            if(path.supplies > 0) {
-                if(path.producing.product == true && path.producing.research == true) {
-                    if(path.product < path.productMax && path.research < path.researchMax) {
-                        path.product += 1;
-                        path.research += 1;
-                        path.supplies -= 2;
-                    } else if(path.product < path.productMax && path.research !< path.researchMax) {
-                        path.product += 1;
-                        path.supplies -= 1;
-                    } else if(path.product !< path.productMax && path.research < path.researchMax) {
-                        path.research += 1;
-                        path.supplies -= 1;
-                    }
-                } else if(path.producing.product == true && path.producing.research == false) {
-                    if(path.product < path.productMax) {
-                        path.product += 1;
-                        path.supplies -= 1;
-                    }
-                } else if(path.producing.product == false && path.producing.research == true) {
-                    if(path.research < path.researchMax) {
-                        path.research += 1;
-                        path.supplies -= 1;
-                    }
-                }
-            }
-            
-            userInfo.bunker = path;
+    if (!running || !userInfo.bunker.owned || userInfo.bunker.supplies <= 0) return;
+
+    const path = userInfo.bunker;
+
+    if (path.producing.product && path.producing.research) {
+        if (path.product < path.productMax && path.research < path.researchMax) {
+            path.product++;
+            path.research++;
+            path.supplies -= 2;
+        } else if (path.product < path.productMax) {
+            path.product++;
+            path.supplies--;
+        } else if (path.research < path.researchMax) {
+            path.research++;
+            path.supplies--;
+        }
+    } else if (path.producing.product) {
+        if (path.product < path.productMax) {
+            path.product++;
+            path.supplies--;
+        }
+    } else if (path.producing.research) {
+        if (path.research < path.researchMax) {
+            path.research++;
+            path.supplies--;
         }
     }
 }
 
-const bunkerTick = setInterval(bunkerUpdate, 5 * 60 * 100);
+const cashTick = setInterval(bikerUpdate, cashSpeed);
+const forgeryTick = setInterval(bikerUpdate, forgerySpeed);
+const weedTick = setInterval(bikerUpdate, weedSpeed);
+const methTick = setInterval(bikerUpdate, methSpeed);
+const cokeTick = setInterval(bikerUpdate, cokeSpeed);
+const acidTick = setInterval(bikerUpdate, acidSpeed);
+const bunkerTick = setInterval(bunkerUpdate, bunkerSpeed);
