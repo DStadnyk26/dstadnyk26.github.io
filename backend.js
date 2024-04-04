b22ab29ec51a7970692623a802c55656d7df7804
DStadnyk26 <165746849+DStadnyk26@users.noreply.github.com>
2024-04-04 12:56:29 -0600

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,19 +1,63 @@
 let running = false;
 
-let bunkerStock = 0;
-let bunkerStockMax = 100;
-let bunkerSupplies = 0;
-let bunkerSuppliesMax = 100;
-
-
-while (running) {
-
+export let userInfo = {
+    
+    bunker: {
+        owned: false,
+        product: 0,
+        productMax: 100,
+        research: 0,
+        researchMax: 0,
+        supplies: 0,
+        suppliesMax: 100,
+        upgrades: {
+            equipment: false,
+            staff: false,
+            security: false,
+        },
+        producing: {
+            product: true,
+            research: false,
+        },
+    },
+    
+    
 }
 
-function start(){
-    running = true;
+function bunkerUpdate() {
+    let path = userInfo.bunker;
+    
+    if(running == true) {
+        if(path.owned == true) {
+            if(path.supplies > 0) {
+                if(path.producing.product == true && path.producing.research == true) {
+                    if(path.product < path.productMax && path.research < path.researchMax) {
+                        path.product += 1;
+                        path.research += 1;
+                        path.supplies -= 2;
+                    } else if(path.product < path.productMax && path.research !< path.researchMax) {
+                        path.product += 1;
+                        path.supplies -= 1;
+                    } else if(path.product !< path.productMax && path.research < path.researchMax) {
+                        path.research += 1;
+                        path.supplies -= 1;
+                    }
+                } else if(path.producing.product == true && path.producing.research == false) {
+                    if(path.product < path.productMax) {
+                        path.product += 1;
+                        path.supplies -= 1;
+                    }
+                } else if(path.producing.product == false && path.producing.research == true) {
+                    if(path.research < path.researchMax) {
+                        path.research += 1;
+                        path.supplies -= 1:
+                    }
+                }
+            }
+            
+            userInfo.bunker = path;
+        }
+    }
 }
 
-function stop(){
-    running = false
-}
+const bunkerTick = setInterval(bunkerUpdate, 5 * 60 * 100);
