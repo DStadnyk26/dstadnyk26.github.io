04984b37ae517148415a03ee427fd4acecf670c1
DStadnyk26 <165746849+DStadnyk26@users.noreply.github.com>
2024-04-08 14:12:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 let running = false;
 let forgeryExpanded = false;
 
-function toggleCollapsibleForgery {
-    if(forgeryExpanded == false) {
-        document,getElementByClassName("forgery-content").style.display = "block";
-        document,getElementByClassName("forgery-content").style.overflow = "visible";
+function toggleCollapsible(let name) {
+    if(document.getElementById(name).style.display == "none") {
+        document.getElementById(name).style.display = "block";
+        document.getElementById(name).style.overflow = "visible";
 
-        forgeryExpanded = true;
-    } else if(forgeryExpanded == true) {
-        document.getElementByClassName("forgery-content").style.display = "none";
-        document.getElementByClassName('forgery-content").style.display = "hidden";
-
-        forgeryExpanded = false;
+    } else if(document.getElementById(name).style.display == "block") {
+        document.getElementById(name).style.display = "none";
+        document.getElementById(name).style.overflow = "hidden";
     }
 }
