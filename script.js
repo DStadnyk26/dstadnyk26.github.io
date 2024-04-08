e50d7252138cabbeb62ab118b21c736ff74e9dfc
DStadnyk26 <165746849+DStadnyk26@users.noreply.github.com>
2024-04-08 13:25:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let running = false;
+let forgeryExpanded = false;
 
-function toggleDropdown(sectionID) {
-    //Toggle Dropdown Content
+function toggleCollapsibleForgery {
+    if(forgeryExpanded == false) {
+        document,getElementByClassName("forgery-content").style.display = "block";
+        document,getElementByClassName("forgery-content").style.overflow = "visible";
+
+        forgeryExpanded = true;
+    } else if(forgeryExpanded == true) {
+        document.getElementByClassName("forgery-content").style.display = "none";
+        document.getElementByClassName('forgery-content").style.display = "hidden";
+
+        forgeryExpanded = false;
+    }
 }
-
-function 
